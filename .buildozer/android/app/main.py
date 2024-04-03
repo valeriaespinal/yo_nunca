3932196a30d5d9d971eefddedfd03ea837212631
valeriaespinal <valeria.espinal@upb.edu.co>
2024-04-03 10:47:58 -0500

    version4

--- .buildozer/android/app/main.py
+++ .buildozer/android/app/main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.graphics import Color, Rectangle
 import random
 
 class YoNuncaApp(App):
